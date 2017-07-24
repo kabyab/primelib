40794e86c9f760e1aeb55c9ec08b4b5c4c6932f7
kabyab <bandokabs@gmail.com>
2017-07-24 19:50:31 +0530

    Initial Project Version

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 24 19:17:03 2017
+
+@author: kabya94
+"""
+
+from distutils.core import setup
+
+setup (
+        name = 'primelib',
+        version = '1.0.0',
+        py_modules = ['primelib'],
+        author = 'kabyaban',
+        author_email = 'bandokabs@gmail.com',
+        url = 'https://github.com/kabyab/',
+        description = 'A library for implementing and using prime numbers.',
+      )
